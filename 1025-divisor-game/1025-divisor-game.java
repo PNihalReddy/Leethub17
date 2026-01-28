a5bcd8de7c6ce7be1d3af356a922f27603059c4b
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-01-28 18:10:14 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39.88%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public boolean divisorGame(int n) 
+    {
+        if(n%2==0)
+        {
+            return true;
+        }    
+        else
+        {
+            return false;
+        }
+    }
+}
