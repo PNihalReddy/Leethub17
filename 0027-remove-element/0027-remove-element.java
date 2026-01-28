2679ea709de2025bdc0580f9f180ade89b9317c9
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-01-28 22:26:31 +0530

    Time: 0 ms (100%), Space: 43.6 MB (33.51%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        int i,c=0;
+
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[c]=nums[i];
+                c++;
+            }
+        }
+
+        return c;
+
+    }
+}
