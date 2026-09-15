8b2446fb20cad6fee4a71fee8c3a03ba57f07202
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-09-15 19:01:04 +0530

    Time: 3 ms (93.54%), Space: 42.5 MB (77.05%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int sumOfMultiples(int n) 
+    {
+        int i,s1=0;
+
+        for(i=1;i<=n;i++)
+        {
+            if(i%3==0 || i%5==0 || i%7==0)
+            {
+                s1+=i;
+            }
+        }
+
+        return s1;
+
+    }
+}
